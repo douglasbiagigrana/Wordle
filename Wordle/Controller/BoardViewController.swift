966dda203659f83c35e75d0c5ce909aefeb6b6c1
douglas <lupim2008@gmail.com>
2022-03-09 09:23:12 -0300

    :building_construction: Add MVC

---
+++ Wordle/Controller/BoardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BoardViewController.swift
+//  Wordle
+//
+//  Created by Douglas Biagi Grana on 09/03/22.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
---
+++ Wordle/Controller/KeyboardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  KeyboardViewController.swift
+//  Wordle
+//
+//  Created by Douglas Biagi Grana on 09/03/22.
+//
+
+import UIKit
+
+class KeyboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
